54ff31178d1ad423fb8fc481edcbf8dc44993877
Adam Jirkovsky <jirkoada@fel.cvut.cz>
2024-01-11 13:49:48 +0100

    Add data processing pipeline and testing

---
+++ tests/test_data.py
@@ -0,0 +1,31 @@
+import pytest
+import os.path
+from datasets import load_from_disk
+from tests import _PATH_DATA
+
+
+def _check_dataset(dataset, split):
+    labels = set()
+    for example in dataset:
+        assert isinstance(example["text"], str) ,f"Invalid input type found in {split} dataset"
+        assert isinstance(example["label"], int), f"Invalid label type found in {split} dataset"
+        assert example["label"] in [0, 1, 2], f"Invalid label found in {split} dataset"
+        labels.add(example["label"])
+    return labels
+
+
+@pytest.mark.skipif(not os.path.exists(_PATH_DATA), reason="Data files not found")
+def test_data():
+    assert os.path.exists(os.path.join(_PATH_DATA, "processed")), "Processed data folder not found"
+    assert os.path.exists(os.path.join(_PATH_DATA, "processed", "train")), "Processed train data folder not found"
+    assert os.path.exists(os.path.join(_PATH_DATA, "processed", "test")), "Processed test data folder not found"
+    assert os.path.exists(os.path.join(_PATH_DATA, "processed", "val")), "Processed val data folder not found"
+
+    train_set = load_from_disk(os.path.join(_PATH_DATA, "raw", "train"))
+    val_set = load_from_disk(os.path.join(_PATH_DATA, "raw", "val"))
+    test_set = load_from_disk(os.path.join(_PATH_DATA, "processed", "test"))
+    
+    labels = _check_dataset(train_set, "train")
+    assert labels == set(range(3)), "Missing labels in the train dataset"
+    _ = _check_dataset(val_set, "validation")
+    _ = _check_dataset(test_set, "test")
